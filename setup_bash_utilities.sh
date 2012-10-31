af4e59a713aa9bdf2a30a0c211822f6387d61a4a
Jim Barritt <jim@planet-ix.com>
2012-10-31 00:08:11 +0000

    [jmdb] Added some standard brews to bash

---
+++ setup_bash_utilities.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Hopefully you have brew installed, if not just do: \nruby -e \"\$(curl -fsSkL raw.github.com/mxcl/homebrew/go)\""
+
+echo "\nOk, now going to install some standard utils\n"
+
+brew install cowsay
+brew install curl
+brew install wget
