d9c091556f696c03596e9f34c2ff2c03d570718d
Jim Barritt <jim@planet-ix.com>
2013-04-03 18:57:20 +0300

    [jmdb] Allowing to specify an extra name aswell as the host name for a shell

--- .emacs.d/jmdb/shell.el
+++ .emacs.d/jmdb/shell.el
@@ -1,12 +1,23 @@
 (defun current-hostname ()
   (first (split-string (shell-command-to-string "hostname") "\n")))
 
-(defun rename-shell-buffer ()
+(defun rename-shell-to-host ()
   (interactive)
   (let ((buffer-name (concat "*shell:" (current-hostname) "*"))) 
     (message (concat "Renaming current buffer to " buffer-name))
     (rename-buffer buffer-name)))
 
+(defun rename-shell (name)
+  "…"
+  (interactive "sEnter name for this shell: ")
+  (let ((buffer-name (concat "*shell:" (current-hostname) ":" name "*"))) 
+    (message (concat "Renaming current buffer to " buffer-name))
+    (rename-buffer buffer-name)))
 
+; turn off shell command echo
+(defun my-comint-init () 
+  (setq comint-process-echoes t)) 
+
+(add-hook 'comint-mode-hook 'my-comint-init) 
 
 
