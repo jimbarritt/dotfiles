7169866645d0f025a25993cdbf49335b36c121d9
Jim Barritt <jim.barritt@gmail.com>
2026-01-02 12:13:57 +0000

    Final fix for treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,59 +1,49 @@
--- Treesitter configuration for syntax highlighting
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  lazy = false,
-  config = function()
-    local configs = require('nvim-treesitter.configs')
-    configs.setup {
-      -- Install parsers for these languages
-      ensure_installed = {
-        'kotlin',
-        'java',
-        'lua',
-        'vim',
-        'vimdoc',
-        'markdown',
-        'cpp',
-        'c',
-        'bash',
-        'python',
-        'git_config'
+  opts = {
+    -- Install parsers for these languages
+    ensure_installed = {
+      'kotlin',
+      'java',
+      'lua',
+      'vim',
+      'vimdoc',
+      'markdown',
+      'cpp',
+      'c',
+      'bash',
+      'python',
+      'git_config'
+    },
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+    -- Automatically install missing parsers when entering buffer
+    auto_install = true,
+    highlight = {
+      enable = true,
+      -- Disable for very large files
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+      additional_vim_regex_highlighting = false,
+    },
+    indent = {
+      enable = true
+    },
+    -- Incremental selection
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<CR>',
+        node_incremental = '<CR>',
+        scope_incremental = '<TAB>',
+        node_decremental = '<S-TAB>',
       },
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      auto_install = true,
-
-      highlight = {
-        enable = true,
-        -- Disable for very large files
-        disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
-        additional_vim_regex_highlighting = false,
-      },
-
-      indent = {
-        enable = true
-      },
-
-      -- Incremental selection
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<CR>',
-          node_incremental = '<CR>',
-          scope_incremental = '<TAB>',
-          node_decremental = '<S-TAB>',
-        },
-      },
-    }
-  end,
+    },
+  },
 }
