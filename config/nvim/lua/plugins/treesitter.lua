6cff5311f2a50d567a6cabc6d132e6fd39c4842c
Jim Barritt <jim.barritt@gmail.com>
2026-01-02 11:42:51 +0000

    Trying to fix treesitter config

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,6 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   config = function()
-  lazy = false
     local configs = require('nvim-treesitter.configs')
     configs.setup {
       -- Install parsers for these languages
