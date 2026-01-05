653923b32a5873af92c20b5d8e9d9095e088c3d9
Jim Barritt <jim.barritt@gmail.com>
2026-01-05 22:23:39 +0000

    Fixing statusline

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,17 +1,36 @@
 return {
   {
     "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = function()
       local colors = {
-        green = "#00ff00",        -- bright green for mode
-        dark_green = "#00aa00",   -- text color
-        forest = "#008000",       -- visual mode
-        bg_green = "#1a3a1a",     -- dark green background
+        green = "#00ff00",
+        dark_green = "#00aa00",
+        forest = "#008000",
+        bg_green = "#1a3a1a",
         black = "#000000",
         gray = "#808080",
       }
+
+      local mode = function()
+        local mode_map = {
+          ['n']  = 'N',
+          ['i']  = 'I',
+          ['v']  = 'v',
+          ['V']  = 'V',
+          [''] = 'B',
+          ['c']  = 'C',
+          ['r']  = 'R',
+          ['t']  = 'T',
+        }
+        local ok, m = pcall(function() return vim.api.nvim_get_mode().mode end)
+        if not ok then return '' end
+        return mode_map[m] or m
+      end
+
+      local lsp_status = function()
+        return "LSP"
+      end
 
       local green_theme = {
         normal = {
@@ -41,21 +60,6 @@ return {
         },
       }
 
-      local mode = function()
-        local mode_map = {
-          ['n']  = 'N',
-          ['i']  = 'I',
-          ['v']  = 'v',
-          ['V']  = 'V',
-          [''] = 'B',
-          ['c']  = 'C',
-          ['r']  = 'R',
-          ['t']  = 'T',
-        }
-        local m = vim.api.nvim_get_mode().mode
-        return mode_map[m] or m
-      end
-
       return {
         options = {
           theme = green_theme,
@@ -66,28 +70,10 @@ return {
         sections = {
           lualine_a = { mode },
           lualine_b = {},
-          lualine_c = { 
-            { 
-              "filename", 
-              path = 0,
-              symbols = {
-                modified = '[+]',
-                readonly = '',
-                unnamed = '[No Name]',
-              }
-            }
-          },
+          lualine_c = { "filename" },
           lualine_x = {},
           lualine_y = { "location" },
           lualine_z = { "progress" },
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = { "filename" },
-          lualine_x = { "location" },
-          lualine_y = {},
-          lualine_z = {},
         },
       }
     end,
